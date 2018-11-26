7c084c3c847bb79baf3da66addc1d0f86b957d6c
Brandon Wirz <brandonwirz@Brandons-MacBook-Pro.local>
2018-11-25 20:41:33 -0700

    guitar virtuoso quiz app in jQuery

---
+++ data.js
@@ -0,0 +1,50 @@
+const quizData = [
+    {
+      questionNum: 1,
+      question: 'What guitarist Played in WhiteSnake, The David Lee Roth Band and has been credited as one of the greatest guitar players of all time?',
+      answer: ['Zakk Wylde', 'Joe Satriani', 'Eddie Van Halen', 'Steve Vai'],
+        correctAnswer: 'Steve Vai',
+      image: './images/steve-vai.jpg',
+      alt: 'Steve Vai photo with guitar'
+    },
+    {
+      questionNum: 2,
+      question: 'Which guitarist was born in the Netherlands, started out playing the piano and plays with his brother in a very popular band?',
+      answer: ['Joe Satriani', 'Eddie Van Halen', 'Steve Vai', 'Zakk Wylde',],
+        correctAnswer: 'Eddie Van Halen',
+      image: './images/evh.jpg',
+      alt: 'Eddie Van Halen photo with guitar'
+    },
+    {
+      questionNum: 3,
+      question: 'This guitarist gave Steve Vai and Kirk Hammit guitar lessons.  He also played with Spinal Tap, Deep Purple and was invited to tour with Mick Jagger on his solo tour?',
+      answer: ['Zakk Wylde', 'Eddie Van Halen', 'Joe Satriani', 'Steve Vai'],
+        correctAnswer: 'Joe Satriani',
+      image: './images/joe-satriani.jpg',
+      alt: 'Joe Satriani photo with guitar'
+    },
+    {
+      questionNum: 4,
+      question: 'This Swedish classical influenced guitarist who performed with Alcatrazz is known for his neo-classical, virtuoso guitar playing?',
+      answer: ['Zakk Wylde', 'Joe Satriani', 'Yngwie Malmsteen', 'Steve Vai'],
+        correctAnswer: 'Yngwie Malmsteen',
+      image: './images/yngwie.jpg',
+      alt: 'Yngwie Malmsteen'
+    },
+    {
+      questionNum: 5,
+      question: 'This famed guitarist has played with Ozzy Osbourne on and off for over 20 years and has been been inducted into the Hollywood Rock Walk of Fame.',
+      answer: ['Zakk Wylde', 'Eddie Van Halen', 'Yngwie Malmsteen', 'Randy Rhoads'],
+        correctAnswer: 'Zakk Wylde',
+      image: './images/zakk-wylde.jpg',
+      alt: 'Zakk Wylde photo with guitar'
+    },
+    {
+      questionNum: 6,
+      question: 'Hard rock, inspirational, neo classical guitarist who performed with Ozzy Ozbourne and tragically died in a plane crash?',
+      answer: ['Zakk Wylde', 'Randy Rhoads', 'Yngwie Malmsteen', 'Steve Vai'],
+        correctAnswer: 'Randy Rhoads',
+      image: './images/randy-rhoads.jpg',
+      alt: 'Randy Rhoads photo with guitar'
+    },
+];
