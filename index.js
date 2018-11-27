7d3a7ee5bf5ff297d4f3857cb17a77df21b1e1b7
Brandon Wirz <brandonwirz@Brandons-MacBook-Pro.local>
2018-11-26 22:52:07 -0700

    updating css and jQuery almost done

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ function startQuiz() {
    $('.quiz-start').on('click', function(event) {
    $('.quiz-start').remove();
    $('.jq-form').css('display', 'block')
-});
+   $('.question-num').text(1);
+  });
 }
 
 function createHTMLQuestion() {
-  //creates queston in the DOM
+  //creates questions in the DOM
   $('.jq-form').html(renderHTML());
 
 }
@@ -22,53 +23,58 @@ function renderHTML() {
   if (selectedQuestion < quizData.length) {
     //creates form with question and multiple choice answers
     //<!-- <legend>lorem ipsum</legend> -->
+    // <legend></legend>
 
     return `
     <h2>${quizData[selectedQuestion].question}</h2>
-
     <form class="styled-form">
       <fieldset>
-          <label class="answerNum">
-          <input type="radio" value="${quizData[selectedQuestion].answer[0]}" name="answer" required>
+          <label for="answer-num1">
+          <input type="radio" value="${quizData[selectedQuestion].answer[0]}" name="answer" id="answer-num1" required>
           <span>${quizData[selectedQuestion].answer[0]}</span>
           </label>
-          <label class="answerNum">
-          <input type="radio" value="${quizData[selectedQuestion].answer[1]}" name="answer" required>
+          <label for="answer-num2">
+          <input type="radio" value="${quizData[selectedQuestion].answer[1]}" name="answer" id="answer-num2" required>
           <span>${quizData[selectedQuestion].answer[1]}</span>
           </label>
-          <label class="answerNum">
-          <input type="radio" value="${quizData[selectedQuestion].answer[2]}" name="answer" required>
+          <label for="answer-num3">
+          <input type="radio" value="${quizData[selectedQuestion].answer[2]}" name="answer" id="answer-num3" required>
           <span>${quizData[selectedQuestion].answer[2]}</span>
           </label>
-          <label class="answerNum">
-          <input type="radio" value="${quizData[selectedQuestion].answer[3]}" name="answer" required>
+          <label for="answer-num4">
+          <input type="radio" value="${quizData[selectedQuestion].answer[3]}" name="answer" id="answer-num4" required>
           <span>${quizData[selectedQuestion].answer[3]}</span>
           </label>
-          <button type="submit" class="submit-button">Submit</button>
+          <button role="button" type="submit" class="submit-button">Submit</button>
       </fieldset>
-    </form>
-      <div id="status-bar">
-      <span id="question-count">Question: ${quizData[selectedQuestion].questionNum}/6</span>
-      <span id="score-count">Score: ${scoreUpdate}/${selectedQuestion}</span>
-    </div>`
+    </form>`
+
   } else {
     quizResultsPage();
     restart();
+    $('.question-num').text(6)
   }
+}
+function newQuestion () {
+  selectedQuestion++;
+  $('.question-num').text(selectedQuestion++);
+}
+function changeScore () {
+  scoreUpdate ++;
 }
 
 //user selects answer on submit run user feedback
 function answer() {
     $('form').on('submit', function (event) {
     event.preventDefault();
-    let selected = $('input:checked');
-    let answer = selected.val();
+    let inputSelected = $('input:checked');
+    let answerSelected = inputSelected.val();
     let correctAnswer = `${quizData[selectedQuestion].correctAnswer}`;
-      if (answer === correctAnswer) {
-        selected.parent().addClass('correct');
+      if (answerSelected === correctAnswer) {
+        inputSelected.parent().addClass('correct');
         isCorrectAnswer();
       } else {
-        selected.parent().addClass('wrong');
+        inputSelected.parent().addClass('wrong');
         isWrongAnswer();
       }
   });
@@ -86,7 +92,7 @@ function isWrongAnswer() {
 function userFeedbackIsCorrect() {
   // the feedback for right answer
   let correctAnswer = `${quizData[selectedQuestion].correctAnswer}`;
-     $('.jq-form').html(`<div class="displayed-content correct-answer"><p class="red">Correct! You got it right!</p><div class="correctAnswerImg">
+     $('.jq-form').html(`<div class="displayed-content correct-answer"><p class="red">Correct! You got it right!</p><div class="">
      <img src="${quizData[selectedQuestion].image}" alt="You got it right!"/></div><p class="guitarist-name">"${correctAnswer}"</p>
      <button type="button" class="next-button">Next Question</button>
      </div>`);
@@ -95,13 +101,13 @@ function userFeedbackIsCorrect() {
 function userFeedbackIsWrong() {
   // the feedback for wrong answer
   let correctAnswer = `${quizData[selectedQuestion].correctAnswer}`;
-     $('.jq-form').html(`<div class="wrong-answer"><p class="red">Sorry! You got it wrong!</p><div class="correctAnswerImg"><img src="${quizData[selectedQuestion].image}" alt="You got it wrong!"/></div><p class="guitarist-name">"${correctAnswer}"</p>
+     $('.jq-form').html(`<div class="wrong-answer"><p class="red">Sorry! You got it wrong!</p><div class=""><img src="${quizData[selectedQuestion].image}" alt="You got it wrong!"/></div><p class="guitarist-name">"${correctAnswer}"</p>
      <button type="button" class="next-button">Next Question</button>
      </div>`);
 }
-function newQuestion () {
-  selectedQuestion++;
-  $('.selectedQuestion').text(selectedQuestion + 1);
+function updateScore () {
+  changeScore();
+  $('.my-score').text(scoreUpdate);
 }
 
 function loadNextQuestion () {
@@ -112,18 +118,10 @@ function loadNextQuestion () {
  });
 }
 
-function changeScore () {
-  scoreUpdate++;
-}
-
-function updateScore () {
-  changeScore();
-  $('.my-score').text(scoreUpdate);
-}
-
 function quizResultsPage() {
   if (scoreUpdate >= 4) {
     $('.jq-form').html(`<div class="results correct-answer"><h3>Good Job!</h3><img class="" src="" alt="" role="img"/><p>You got ${scoreUpdate} / 6</p><p>Excellent job!</p><button class="restartButton">Restart Quiz</button></div>`);
+
   } else if (scoreUpdate < 1 && scoreUpdate >= 3) {
     $('.jq-form').html(`<div class="results correct-answer"><h3>Good Job!</h3><img class="" src="" alt="" role="img"/><p>You got ${scoreUpdate} / 6</p><p>not too bad!</p><button class="restartButton">Restart Quiz</button></div>`);
   }
@@ -131,12 +129,13 @@ function quizResultsPage() {
     $('.jq-form').html(`<div class="results correct-answer"><h3></h3><img class="" src="" alt="" role="img"/><p>You got ${scoreUpdate} / 6</p><p>You didn't do so well, want to try again? </p><button class="restartButton">Restart Quiz</button></div>`);
    }
 }
+
 function restart() {
   $('main').on('click', '.restartButton', function (event) {
    location.reload();
  });
-
 }
+
 function createQuiz () {
   startQuiz();
   createHTMLQuestion();
